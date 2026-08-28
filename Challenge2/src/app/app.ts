5c0b0fd01d46945e3d71670d50a29cfe876d9415
miguelararat <miguel.ararat@uao.edu.co>
2026-08-28 18:26:54 -0500

    Creación de los componente del proyecto e implementación de algunos (el array que lo almacena y el "objeto" de cada contacto)

--- Challenge2/src/app/app.ts
+++ Challenge2/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Contact } from './contact.model';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Challenge2');
+
+  public contacts = signal<Contact[]>([]);
+  public isLoading = signal<boolean> ( true );
+
+  constructor(){
+  setTimeout(()=> {
+    this.contacts.set([
+      {id: 1, name: "Juan", phone: "3203206060"}
+    , {id: 2, name: "Daniel" , phone : "3213217070"}])
+    this.isLoading.set(false);
+    
+  }, 2000);
+  }
 }
---
+++ Challenge2/src/app/contact-item/contact-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-item',
+  imports: [],
+  templateUrl: './contact-item.html',
+  styleUrl: './contact-item.css',
+})
+export class ContactItem {}
