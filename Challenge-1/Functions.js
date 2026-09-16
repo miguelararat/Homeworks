2f366f5cc2ab6e2a32f41d5133af39e394f9fcbc
miguelararat <miguel.ararat@uao.edu.co>
2026-09-15 22:58:44 -0500

    Challenge 1 finalizado

---
+++ Challenge-1/Functions.js
@@ -0,0 +1,23 @@
+function isEvenOrOdd(a){
+    if( a % 2 == 0){
+        console.log("This number is Even");
+    }
+    else{
+        console.log("This number is Odd");
+    }
+}
+
+const isEvenOrOddArrow = (a) => {
+    if (a % 2 == 0) {
+        console.log("This number is Even");
+    } else {
+        console.log("This number is Odd");
+    }
+};
+
+
+isEvenOrOdd(4);        
+isEvenOrOddArrow(4);   
+
+isEvenOrOdd(7);        
+isEvenOrOddArrow(7); 
